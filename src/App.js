82e1b504192c7cc611527efbb8006dad936d9ef1
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-06-23 22:09:13 +0300

    feat: Collect emails in firestore.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { createSbscription } from './db/firestore.api';
 function App() {
 
     const submit = (text) => {
-        console.log(text);
+        createSbscription(text);
     }
 
     return (
--- src/db/firestore.api.js
+++ src/db/firestore.api.js
@@ -2,15 +2,25 @@ import db from "./firestore.config";
 import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore'
 
 // Collection ref
-const colRef = collection(db, 'newsletter');
+// const collectionReference = collection(db, 'newsletter');
 
 // Get collection data
-getDocs(colRef).then(snapshot => {
-    let issues = [];
-    snapshot.docs.forEach(doc => {
-        issues.push({ ...doc.data(), id: doc.id });
+// getDocs(collectionReference).then(snapshot => {
+//     let issues = [];
+//     snapshot.docs.forEach(doc => {
+//         issues.push({ ...doc.data(), id: doc.id });
+//     });
+//     console.log(issues);
+// }).catch(err => {
+//     console.log(err.message);
+// });
+
+export const createSbscription = async (text)=> {
+    const collectionReference = collection(db, 'newsletter');
+    console.log("email: " + text);
+    addDoc(collectionReference, {
+        channel: "en",
+        email: text,
+        state: true
     });
-    console.log(issues);
-}).catch(err => {
-    console.log(err.message);
-});
+};
