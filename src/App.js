6e23e9c3e9f4c5053be101983198a4e7699a300e
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-06-23 16:55:03 +0300

    feat: Project cleanup.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="newsletter-inner-wrapper">
+            <h1>Hello</h1>
+            <form>
+                <input type="email" />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
 }
 
 export default App;
